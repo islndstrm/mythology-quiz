add12be53c392fac61b9eac5c37b3f2f04ed6a12
Liz E <lizetchells@gmail.com>
2018-05-24 22:50:35 -0400

    fixing errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var counter = 0;
 var right = [];
 var wrong = [];
+var message = "";
 var quiz = [
   ["Who is god of roadways, travelers, merchants and thieves?", "hermes"],
   ["Who is the goddess of the moon, the hunt, and young maidens?", "artemis"],
@@ -10,26 +11,34 @@ var quiz = [
   ["Who is the goddess of war, wisdom and crafts?", "athena"],
   ["What is the roman name for Zeus?", "jupiter"],
   ["Who are Leto's two twins?", "apollo and artemis"],
-  ["Who is the god of wine?", "dionysus"],
-  ["Who is the goddess of victory?", "nike"],                                                                        ];                                                                                                                                                                                                                                  function print(message) {
+  ["Who is the god of wine?", "Dionysus"],
+  ["Who is the goddess of victory?", "Nike"]
+];                                                                                                                                                                                                                                                                                                    function print(message) {
+
+function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
-
-  //document.write(message);
 }
 
-for (var i=0; i < quiz.length; i++) {
-  var answer = prompt(quiz[i][0]);
-  answer = answer.toLowerCase();
-  if (answer === quiz[i][1]) {
-    counter += 1;
-    right.push("<li>" + quiz[i][0] + "</li>");
-  } else {
-    wrong.push("<li>" + quiz[i][0] + "</li>");
-  }
+for (let i = 0; i < quiz.length; i++) {
+    message += "<p>" + quiz[i][0] + "</p>";
 }
 
+print(message);
 
-print("<p>You got " + counter + " question(s) right!</p>" + "<p><strong>You got these questions correct:</strong></p><ol>" + right.join(' ') + "</ol><p><strong>You got these questions wrong:</strong></p><ol>" + wrong.join(' ') + "</ol>");
+//
+// for (var i=0; i < quiz.length; i++) {
+//   var answer = prompt(quiz[i][0]);
+//   answer = answer.toLowerCase();
+//   if (answer === quiz[i][1]) {
+//     counter += 1;
+//     right.push("<li>" + quiz[i][0] + "</li>");
+//   } else {
+//     wrong.push("<li>" + quiz[i][0] + "</li>");
+//   }
+// }
+
+
+//print("<p>You got " + counter + " question(s) right!</p>" + "<p><strong>You got these questions correct:</strong></p><ol>" + right.join(' ') + "</ol><p><strong>You got these questions wrong:</strong></p><ol>" + wrong.join(' ') + "</ol>");
 //print("<p><strong>You got these questions correct:</strong></p>");
 //print("<p><strong>You got these questions wrong:</strong></p>");
